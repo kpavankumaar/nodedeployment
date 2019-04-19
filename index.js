7753a19fb45beb3a597c13d2641a304cdaac2deb
ngjsmdb <ngjsmdb@gmail.com>
2019-04-19 18:10:08 +0530

    fixed port error

--- index.js
+++ index.js
@@ -37,5 +37,5 @@ app.use('/api/products',productRouter);
 app.use('/api/reviews',reviewRouter);
 
 app.listen(PORT,function(){
-    console.log('we are listening to port', Port );
+    console.log('we are listening to port', PORT );
 });
